278fe3968fecf97310056412ae82a3be3a1ae64b
Lukas Pirl <git@lukas-pirl.de>
2014-06-24 12:35:23 +0200

    added plugin meta data

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_scm_hookhelpers do
+  name 'Redmine Scm Hookhelpers plugin'
+  author 'Lukas Pirl'
+  author_url 'http://lukas-pirl.de/'
+  description 'This Plugin provides helpers for commit hooks when using the Redmine SCM plugin.'
+  version '0.0.1'
+  url 'https://github.com/lpirl/redmine_scm_hookhelpers'
+end
