712d4bde61bf2cb226cadd99e57b78e535b312bb
Lukas Pirl <git@lukas-pirl.de>
2014-06-24 15:16:54 +0200

    added possibility to fetch changesets for repositories by root_url   via WS

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
+require 'redmine'
+
 Redmine::Plugin.register :redmine_scm_hookhelpers do
   name 'Redmine Scm Hookhelpers plugin'
   author 'Lukas Pirl'
@@ -5,4 +7,9 @@ Redmine::Plugin.register :redmine_scm_hookhelpers do
   description 'This Plugin provides helpers for commit hooks when using the Redmine SCM plugin.'
   version '0.0.1'
   url 'https://github.com/lpirl/redmine_scm_hookhelpers'
+  requires_redmine :version_or_higher => '2.5.0'
+end
+
+Rails.configuration.to_prepare do
+  SysController.send(:include, SysControllerPatch)
 end
---
+++ lib/sys_controller_patch.rb
@@ -0,0 +1,39 @@
+require_dependency 'sys_controller'
+
+module SysControllerPatch
+
+    def self.included(base)
+        base.extend(ClassMethods)
+        base.send(:include, InstanceMethods)
+        base.class_eval do
+            alias_method_chain :fetch_changesets, :by_root_url
+        end
+    end
+
+    module ClassMethods
+    end
+
+    module InstanceMethods
+
+        def fetch_changesets_with_by_root_url
+            unless params[:root_url]
+                return fetch_changesets_without_by_root_url
+            end
+
+            repositories = Repository.find_all_by_root_url params[:root_url].to_s
+
+            unless repositories
+                render :nothing => true, :status => 404
+            end
+
+            for repository in repositories
+                repository.fetch_changesets
+                Rails.logger.info "fetched changesets for " + repository.root_url
+            end
+
+            render :nothing => true, :status => 200
+        end
+
+    end
+
+end
