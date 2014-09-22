82f065c93775856042d53e352e65a6f075c7acc4
Lukas Pirl <github@lukas-pirl.de>
2014-09-22 12:07:55 +0200

    made API call asynchronous

--- example_installation/redmine_svn_post_update_hook.sh
+++ example_installation/redmine_svn_post_update_hook.sh
@@ -8,5 +8,11 @@ WS_KEY=xy42xy42xy42xy42
 SVN_ROOT=file:///var/repositories/svn
 SVN_REPO=`basename "$1"`
 
-wget -O/dev/null --quiet \
-    "${REDMINE_URL}/sys/fetch_changesets?root_url=${SVN_ROOT}/${SVN_REPO}&key=${WS_KEY}"
+# make the API call asynchronous so users don't have to wait for it to
+#  return upon commit:
+#     * nohup prevents wget to receive SIGHUP and similar
+#     * '>/dev/null 2>&1 &' detatches STDIN *and* STDERR
+
+nohup wget -O/dev/null --quiet \
+    "${REDMINE_URL}/sys/fetch_changesets?root_url=${SVN_ROOT}/${SVN_REPO}&key=${WS_KEY}" \
+    >/dev/null 2>&1 &
